0405c8f3488ac79d68967c26675b7fcc067c6a89
Shlomy Trajber <shlomytra@gmail.com>
2018-01-09 18:04:36 +0200

    Implemented Player and printer

--- othello/src/ConsolePrinter.java
+++ othello/src/ConsolePrinter.java
@@ -4,14 +4,14 @@ public class  ConsolePrinter implements Printer {
     @Override
     public void printNextTurn(boolean blacksTurn) {
         if (blacksTurn)
-            System.out.println("The next move - Player x:\n\n");
+            System.out.println("The next move - Player x:\n");
         else
-            System.out.println("The next move - Player o:\n\n");
+            System.out.println("The next move - Player o:\n");
     }
 
     @Override
     public void printNoMoves() {
-        System.out.println("No possible moves, the turn goes to the next player. Press a key and then enter to continue\n");
+        System.out.println("No possible moves, the turn goes to the next player. Press a key and then enter to continue");
         Scanner scanner = new Scanner(System.in);
         char k = scanner.next().charAt(0);
     }
@@ -19,33 +19,63 @@ public class  ConsolePrinter implements Printer {
     @Override
     public void printLastMove(boolean blacksTurn, int i, int j) {
         if (!blacksTurn)
-            System.out.println("The last move of player x was: (" + i + 1 + "," + j + 1 + ")\n");
+            System.out.println("The last move of player x was: (" + i + 1 + "," + j + 1 + ")");
         else
-            System.out.println("The last move of player o was: (" + i + 1 + "," + j + 1 + ")\n");
+            System.out.println("The last move of player o was: (" + i + 1 + "," + j + 1 + ")");
     }
 
     @Override
     public void printBoard(Board b, boolean onlyPrintBoard) {
-
+        if (!onlyPrintBoard)
+            System.out.println("The current board:");
+        int row = 1;
+        String tmp = ("-"*4*b.getBoardSize());
+        System.out.print( " | ");
+        for (int i = 1; i < b.getBoardSize() + 1; i++)
+            System.out.print(i + " | ");
+        System.out.print("\n" + tmp + "--");
+        for (int i = 0; i < b.getBoardSize(); i++, row++) {
+            System.out.println(row + "| ");
+            for (int j = 0; j < b.getBoardSize(); j++)
+                System.out.print(b.getCell(i,j) << " | ");
+            System.out.println("\n" + tmp + "--");
+        }
+        if (!onlyPrintBoard)
+            System.out.println("The score is: Black = " + b.getBTiles() << "\tWhite = " + b.getWTiles());
     }
 
     @Override
     public void printsWhoWon(Board b) {
-
+        System.out.println("The final board:\n");
+        printBoard(b, true);
+        System.out.println("The game is finished! The final score is: Black = " << b.getBTiles() << "\tWhite = "
+                << b.getWTiles() << endl);
+        cout << "The winner is ";
+        if (b.getBTiles() > b.getWTiles())
+            cout << "Black!\n";
+        else if (b.getBTiles() < b.getWTiles())
+            cout << "White!\n";
+        else
+            cout << "no one! The game ended as a draw!\n";
     }
 
     @Override
     public void printEnterNextTurn() {
-
+        System.out.println("Please enter your next move: row,col");
     }
 
     @Override
     public void PrintInvalidMove() {
-
+        System.out.println("Invalid move, please enter a legal move: row,col");
     }
 
     @Override
     public void printPosMoves(Board b) {
-
+        System.out.print("Your possible moves are: ");
+        for (int i = 0; i < b.getBoardSize(); i++)
+            for (int j = 0; j < b.getBoardSize(); j++)
+                if (b.getPosCell(i, j))
+                    System.out.print("(" + i + 1 + ","+ j + 1 + ") ");
+        System.out.println();
     }
 }
--- othello/src/Player.java
+++ othello/src/Player.java
@@ -1,2 +1,68 @@
-public class Player {
+public abstract class Player {
+    protected GameLogic logic;
+    protected boolean canPlay;
+    protected int type;
+    protected int[] lastMove ;
+
+    /**
+     * @name : Player
+     * @parameters : no parameters
+     * @return : the function creates a new instance of player
+     **/
+
+    Player(GameLogic l){
+    this.logic = l;
+    }
+
+    /**
+     * @name : getCanPlay
+     * @parameters : no parameters
+     * @return : the function returns true if the player has a possible move to play or false otherwise
+     **/
+
+    public boolean getCanPlay(){
+        return canPlay;
+    }
+
+    /**
+     * @name : DeclareNoMoves
+     * @parameters : no parameters
+     * @return : the function notify that the player can't play
+     **/
+
+    virtual void declareNoMoves();
+
+    /**
+     * @name : getLastMove
+     * @parameters : no parameters
+     * @return : the function returns a pair with the index of the last move played
+     **/
+
+    pair<int, int> getLastMove();
+
+    /**
+     * @name : getType
+     * @parameters : no parameters
+     * @return : the function returns the type of the player (HumanPlayer, AIPlayer etc)
+     **/
+
+    int getType();
+
+    /**
+     * @name : playOneTurn
+     * @parameters : true if it's the black's player turn, false otherwise
+     * @return : the function makes a move in the game
+     **/
+
+    virtual void playOneTurn(bool blacksTurn) = 0;
+
+    /**
+     * @name : onlineEnd
+     * @parameters : no parameters
+     * @return : in LocalPlayer only the function sends a message to the server declaring that the current game has ended
+     **/
+
+    virtual void onlineEnd() = 0;
+
+
 }
