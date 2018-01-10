e270776001c7d47abd8efd028509a9166ee7186a
Shlomy Trajber <shlomytra@gmail.com>
2018-01-10 12:15:08 +0200

    Implamented main

--- othello/src/Main.java
+++ othello/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main (String[] args) {
-        System.out.println("Hello world");
+        Board board = new Board(8);
+        Printer printer = new ConsolePrinter();
+        GameLogic logic = new DeafultLogic(board);
+        Player black = new HumanPlayer(logic,printer);
+        Player white = new HumanPlayer(logic,printer);
+        Game game = new Game(board,black,white,logic,printer);
+        game.playGame();
     }
 }
